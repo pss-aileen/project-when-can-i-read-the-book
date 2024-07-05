678e014bc092c1edb5c35636ac627eebc807ecd1
Aileen <140990100+pss-aileen@users.noreply.github.com>
2024-07-05 15:27:15 +0900

    🐛 fix: 入力項目の取得 #6

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,37 @@
 import "./style.css";
+
+const button = document.querySelector("button");
+
+button?.addEventListener("click", (e) => {
+  // ページ遷移のを防ぐ、でも理解不十分。
+  e.preventDefault();
+
+  const infoForm = document.forms.namedItem("info") as HTMLFormElement;
+  const reservedDate = (
+    infoForm.elements.namedItem("reservedDate") as HTMLInputElement
+  ).value;
+  const reservedOrder = (
+    infoForm.elements.namedItem("reservedOrder") as HTMLInputElement
+  ).value;
+  const isSameLibrary =
+    (infoForm.elements.namedItem("isSameLibrary") as RadioNodeList).value ===
+    "true"
+      ? true
+      : false;
+  const holdPeriod = (
+    infoForm.elements.namedItem("holdPeriod") as RadioNodeList
+  ).value;
+  const isOnBusinessDay =
+    (infoForm.elements.namedItem("isOnBusinessDay") as RadioNodeList).value ===
+    "true"
+      ? true
+      : false;
+
+  console.log(
+    reservedDate,
+    reservedOrder,
+    isSameLibrary,
+    holdPeriod,
+    isOnBusinessDay,
+  );
+});
