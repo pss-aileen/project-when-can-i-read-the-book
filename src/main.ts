611d80992f9c9d58db4478871c2d18db412fd9de
Aileen <140990100+pss-aileen@users.noreply.github.com>
2024-07-07 16:36:10 +0900

    🚧 wip: getJapaneseDayを分離 #35

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import getJapaneseDay from "./getJapaneseDay";
 import setTodayDate from "./setTodayDate";
 import "./style.css";
 
@@ -71,35 +72,6 @@ function calculateTotalDaysPerPerson(
   DaysPersonBorrow: number,
 ): number {
   return DaysLibraryDelivery + DaysLibraryHold + DaysPersonBorrow;
-}
-
-function getJapaneseDay(expectedDay: number): string {
-  let day = "";
-  switch (expectedDay) {
-    case 0:
-      day = "日";
-      break;
-    case 1:
-      day = "月";
-      break;
-    case 2:
-      day = "火";
-      break;
-    case 3:
-      day = "水";
-      break;
-    case 4:
-      day = "木";
-      break;
-    case 5:
-      day = "金";
-      break;
-    case 6:
-      day = "土";
-      break;
-  }
-
-  return day;
 }
 
 function renderExpectedDate(
